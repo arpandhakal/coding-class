3706139a9cc4def557d86a0b4e9f34477b91abe1
arpandhakal <arpandhakal20@gmail.com>
2023-09-19 17:14:19 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function count(sentence, countInput) {
+  let sentenceArray = sentence.split(" ");
+  let count = 0;
+  for (i = 0; i < sentence.length; i++) {
+    if (sentenceArray[i] == countInput) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+
+let a = count("my name is subham my surname is subham the", "the");
+document.querySelector("#header").innerHTML = "headasdas";
